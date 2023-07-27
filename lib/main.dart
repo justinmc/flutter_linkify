64e1d95f9439163952ef4b855ed1b68f2c961add
Justin McCandless <jmccandless@google.com>
2023-07-27 10:28:14 -0700

    Update main example to include regex and rich text examples

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 
-// This example demonstrates highlighting URLs in a TextSpan tree instead of a
-// flat String.
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({
-    super.key,
-  });
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,33 +16,26 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter LinkedText.spans Demo'),
+      home: const MyHomePage(title: 'Flutter Link Demo'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({
-    super.key,
-    required this.title,
-  });
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
+  static const String url = 'https://www.example.com';
+  static const String text = 'www.example123.co.uk\nasdf://subdomain.example.net\nsubdomain.example.net\nftp.subdomain.example.net\nhttp://subdomain.example.net\nhttps://subdomain.example.net\nhttp://example.com/\nhttps://www.example.org/\nftp://subdomain.example.net\nexample.com\nsubdomain.example.io\nwww.example123.co.uk\nhttp://example.com:8080/\nhttps://www.example.com/path/to/resource\nhttp://www.example.com/index.php?query=test#fragment\nhttps://subdomain.example.io:8443/resource/file.html?search=query#result\n"example.com"\n\'example.com\'\n(example.com)\nsubsub.www.example.com\nhttps://subsub.www.example.com';
 
-  void _onTapUrl (BuildContext context, String urlString) {
-    final Uri? uri = Uri.tryParse(urlString);
-    if (uri == null) {
-      throw Exception('Failed to parse $urlString.');
+  void _onTapUrl (String urlString) async {
+    Uri uri = Uri.parse(urlString);
+    if (uri.host.isEmpty) {
+      uri = Uri.parse('https://$urlString');
     }
-
-    // A package like url_launcher would be useful for actually opening the URL
-    // here instead of just showing a dialog.
-    Navigator.of(context).push(
-      DialogRoute<void>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(title: Text('You tapped: $uri')),
-      ),
-    );
+    if (!await launchUrl(uri)) {
+      throw 'Could not launch $urlString.';
+    }
   }
 
   @override
@@ -54,39 +44,102 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Builder(
-          builder: (BuildContext context) {
-            return SelectionArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  LinkedText(
-                    onTap: (String urlString) => _onTapUrl(context, urlString),
-                    spans: <InlineSpan>[
-                      TextSpan(
-                        text: 'Check out https://www.',
-                        style: DefaultTextStyle.of(context).style,
-                        children: const <InlineSpan>[
-                          TextSpan(
-                            style: TextStyle(
-                              fontWeight: FontWeight.w800,
-                            ),
-                            text: 'flutter',
-                          ),
-                        ],
+      body: Builder(
+        builder: (BuildContext context) {
+          final List<InlineSpan> spans = <InlineSpan>[
+            TextSpan(
+              text: '$url?q=0\n',
+              style: DefaultTextStyle.of(context).style,
+              children: <InlineSpan>[
+                TextSpan(
+                  text: 'https://www.ex ftp://subdomain.example.com https://www.ex',
+                  children: <InlineSpan>[
+                    TextSpan(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        decoration: TextDecoration.combine(<TextDecoration>[
+                          TextDecoration.lineThrough,
+                          TextDecoration.underline,
+                        ]),
                       ),
-                      TextSpan(
-                        text: '.dev!',
-                        style: DefaultTextStyle.of(context).style,
+                      text: 'ample.com?q=1 ex',
+                    ),
+                    const TextSpan(
+                      text: 'ample.com?q=2',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            TextSpan(
+              text: '\nhttps://',
+              style: DefaultTextStyle.of(context).style,
+              children: <InlineSpan>[
+                TextSpan(
+                  text: 'www.',
+                  children: <InlineSpan>[
+                    TextSpan(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        decoration: TextDecoration.combine(<TextDecoration>[
+                          TextDecoration.lineThrough,
+                          TextDecoration.underline,
+                        ]),
                       ),
-                    ],
+                      text: 'ex',
+                    ),
+                    const TextSpan(
+                      text: 'ample',
+                    ),
+                    const TextSpan(
+                      text: '.com?q=3\nex',
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            TextSpan(
+              style: DefaultTextStyle.of(context).style,
+              text: 'ample.com?q=4',
+            ),
+            TextSpan(
+              style: DefaultTextStyle.of(context).style,
+              text: '\nex',
+            ),
+            TextSpan(
+              style: DefaultTextStyle.of(context).style,
+              text: 'ample.com?q=5',
+            ),
+          ];
+
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                LinkedText(
+                  onTap: _onTapUrl,
+                  spans: spans,
+                ),
+                const SizedBox(
+                  height: 64.0,
+                ),
+                RichText(
+                  text: TextSpan(
+                    children: spans,
                   ),
-                ],
-              ),
-            );
-          },
-        ),
+                ),
+                const SizedBox(
+                  height: 16.0,
+                ),
+                LinkedText(
+                  onTap: _onTapUrl,
+                  text: text,
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
